1efb377b6056cc25dbeebd8b8def6f14552abb11
MartmatiX <martin.malir@gmail.com>
2024-11-26 18:34:26 +0100

    Fix of swagger issues

--- src/main/java/com/example/moneymanager/MoneyManagerApplication.java
+++ src/main/java/com/example/moneymanager/MoneyManagerApplication.java
@@ -8,7 +8,6 @@ public class MoneyManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyManagerApplication.class, args);
-        System.out.println();
     }
 
 }
